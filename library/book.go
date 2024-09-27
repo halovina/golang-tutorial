cdda6cb059e551ed4096ee382653ee795b3890e8
alfin <alfin@linkaja.id>
2024-09-27 09:10:46 +0700

    public and private function

---
+++ library/book.go
@@ -0,0 +1,11 @@
+package library
+
+import "fmt"
+
+func Room() {
+	fmt.Println(" room 1 ")
+}
+
+func books() {
+	fmt.Println(" my books .....")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/halovina/golang-tutorial/library"
+
+func main() {
+	// call public funtion
+	library.Room()
+
+	//call private function
+	library.Mybook()
+}
